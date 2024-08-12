7ab1166ad365c4a5a8e397c387d70bf0c8b10c78
ssimsir <s_simsir@yahoo.com>
2024-08-12 21:36:05 +0300

    token done

--- nodejs/class-notes/27-StockApi/src/models/token.js
+++ nodejs/class-notes/27-StockApi/src/models/token.js
@@ -3,4 +3,34 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- *
+
+{
+    "userId": "65343222b67e9681f937f001",
+    "token": "...tokenKey..."
+  }
+  /* ------------------------------------------------------- */
+//Token model
+
+const TokenSchema = new mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'User',
+        required:true,
+        index:true,
+        unique:true
+    },
+    token:{
+        type:String,
+        required:true,
+        index:true,
+        unique:true
+    }
+
+}, {
+    collation:'tokens',
+    timestamps:true
+})
+
 /* ------------------------------------------------------- */
+module.exports = mongoose.model('Token', TokenSchema)
--- nodejs/class-notes/27-StockApi/src/routes/token.js
+++ nodejs/class-notes/27-StockApi/src/routes/token.js
@@ -4,3 +4,21 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+//routes/token
+
+const token = require('../controllers/token')
+
+//URL : /tokens
+
+router.route('/')
+    .get(token.list)
+    .post(token.create)
+
+router.route('/:id')
+    .get(token.read)
+    .put(token.create)
+    .patch(token.create)
+    .delete(token.delete)
+
+/* ------------------------------------------------------- */
+module.exports=router
