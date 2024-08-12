7d54212a01996683cdbbc25f97ed7b89ed3b9c94
ssimsir <s_simsir@yahoo.com>
2024-08-12 20:50:06 +0300

    user done

--- nodejs/class-notes/27-StockApi/src/routes/user.js
+++ nodejs/class-notes/27-StockApi/src/routes/user.js
@@ -4,3 +4,21 @@
 ------------------------------------------------------- */
 const router = require('express').Router()
 /* ------------------------------------------------------- */
+//routes/user
+
+const user = require('../controllers/user')
+
+//URL : /users
+
+router.route('/')
+    .get(user.list)
+    .post(user.create)
+
+router.route('/:id')
+    .get(user.read)
+    .put(user.create)
+    .patch(user.create)
+    .delete(user.delete)
+
+/* ------------------------------------------------------- */
+module.exports=router
