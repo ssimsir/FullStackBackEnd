e3e23745169aa51494777220e63e338c7fd0aae3
ssimsir <s_simsir@yahoo.com>
2024-08-12 20:26:35 +0300

    user list done

--- nodejs/class-notes/27-StockApi/src/models/user.js
+++ nodejs/class-notes/27-StockApi/src/models/user.js
@@ -3,4 +3,86 @@
     | FULLSTACK TEAM | NODEJS / EXPRESS |
 ------------------------------------------------------- */
 const { mongoose } = require('../configs/dbConnection')
+/* ------------------------------------------------------- 
+
+{
+    "username": "admin",
+    "password": "aA?123456",
+    "email": "admin@site.com",
+    "firstName": "admin",
+    "lastName": "admin",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": true
+}
+{
+    "username": "staff",
+    "password": "aA?123456",
+    "email": "staff@site.com",
+    "firstName": "staff",
+    "lastName": "staff",
+    "isActive": true,
+    "isStaff": true,
+    "isAdmin": false
+}
+{
+    "username": "test",
+    "password": "aA?123456",
+    "email": "test@site.com",
+    "firstName": "test",
+    "lastName": "test",
+    "isActive": true,
+    "isStaff": false,
+    "isAdmin": false
+}
 /* ------------------------------------------------------- */
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        index:true
+    },
+    password:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        required:true,
+        unique:true,
+        index:true
+    },
+    firstName:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    lastName:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    isStaff:{
+        type:Boolean,
+        default:false
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    }
+},{
+    collation:'users',
+    timestamps:true
+})
+/* ------------------------------------------------------- */
+
+module.exports = mongoose.model('User', UserSchema)
